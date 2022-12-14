119f1148f20ec3eaea5813a903b0de31f0e558ff
Vance <vances678@gmail.com>
2022-12-14 00:03:27 -0500

    Refactored checkin slide

---
+++ lib/pages/checkin_slide.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:valyou/data/defaults.dart';
+import 'package:valyou/data/value.dart';
+import 'package:valyou/widgets/controls/buttons/primary_button.dart';
+import 'package:valyou/widgets/controls/buttons/secondary_button.dart';
+import 'package:valyou/widgets/controls/selectors/rating_selector.dart';
+
+class CheckinSlide extends StatefulWidget {
+  const CheckinSlide({
+    super.key,
+    required this.pageController,
+    required this.value,
+    required this.isFirstSlide,
+    required this.isLastSlide,
+  });
+
+  final PageController pageController;
+  final Value value;
+  final bool isFirstSlide;
+  final bool isLastSlide;
+
+  @override
+  State<CheckinSlide> createState() => _CheckinSlideState();
+}
+
+class _CheckinSlideState extends State<CheckinSlide>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Padding(
+      padding: Defaults.padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            widget.value.name,
+            style:
+                GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: Defaults.increment),
+          RatingSelector(
+            maxRating: 5,
+            onSelect: (rating) {
+              //print(rating);
+            },
+          ),
+          const SizedBox(height: Defaults.increment * 2),
+          PrimaryButton(
+            onPressed: (!widget.isLastSlide)
+                ? () {
+                    widget.pageController.animateToPage(
+                      widget.pageController.page!.toInt() + 1,
+                      curve: Curves.easeInOut,
+                      duration: const Duration(milliseconds: 400),
+                    );
+                  }
+                : () {
+                    Navigator.pop(context);
+                  },
+            child: Text(
+              (!widget.isLastSlide) ? "Continue" : "Finish",
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(height: Defaults.increment * 2),
+          (!widget.isFirstSlide)
+              ? SecondaryButton(
+                  onPressed: () {
+                    widget.pageController.animateToPage(
+                      widget.pageController.page!.toInt() - 1,
+                      curve: Curves.easeInOut,
+                      duration: const Duration(milliseconds: 400),
+                    );
+                  },
+                  child: Text(
+                    "Previous",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
